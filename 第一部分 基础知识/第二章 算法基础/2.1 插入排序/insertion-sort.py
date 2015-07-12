b00eabe74df4e334a81ecb91ec5611f40aba6013
PyBeaner <758046738@qq.com>
2015-07-12 22:40:21 +0800

    descending

--- 第一部分 基础知识/第二章 算法基础/2.1 插入排序/insertion-sort.py
+++ 第一部分 基础知识/第二章 算法基础/2.1 插入排序/insertion-sort.py
@@ -10,10 +10,23 @@ def insertion_sort(alist):
         alist[i] = value
 
 
+def insertion_sort_desc(alist):
+    for i in range(1, len(alist)):
+        value = alist[i]
+        while value > alist[i - 1] and i > 0:
+            alist[i] = alist[i - 1]
+            i -= 1
+        alist[i] = value
+
+
 if __name__ == '__main__':
     import random
 
     alist = random.sample(range(10), 10)
     print("original list:", alist)
     insertion_sort(alist)
-    print("sorted list:", alist)
+
+    print("sorted list(ascending):", alist)
+
+    insertion_sort_desc(alist)
+    print("sorted list(descending):", alist)
