b5efa0e391c406b6cec2062b28a17fdf8b1f5224
PyBeaner <758046738@qq.com>
2015-07-13 08:57:29 +0800

    insertion sort with bisection search

---
+++ 第一部分 基础知识/第二章 算法基础/2.3-6 利用二分查找进行插入排序.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from random import sample
+
+__author__ = 'PyBeaner'
+
+
+# 原插入排序
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        value = alist[i]
+        while value < alist[i - 1] and i > 0:
+            alist[i] = alist[i - 1]
+            i -= 1
+        alist[i] = value
+
+
+# 利用二分查找的插入排序
+def insertion_sort_using_bisection(alist):
+    for i in range(1, len(alist)):
+        value = alist[i]
+        pos = bisection_search_position(alist[:i], value)
+        for j in range(i, pos - 1, -1):
+            alist[j] = alist[j - 1]
+        alist[pos] = value
+
+
+def bisection_search_position(alist, target, offset=0):
+    length = len(alist)
+    if length == 1:
+        return offset if alist[0] > target else offset + 1
+    if length == 0:
+        return offset
+
+    mid = length // 2
+    if alist[mid] == target:
+        return mid + offset
+    if alist[mid] > target:
+        return bisection_search_position(alist[:mid], target, offset=offset)
+
+    if alist[mid] < target:
+        offset += mid + 1
+        return bisection_search_position(alist[mid + 1:], target, offset=offset)
+
+
+if __name__ == '__main__':
+    alist = sample(range(20), 10)
+    print(alist)
+    insertion_sort_using_bisection(alist)
+    print(alist)
