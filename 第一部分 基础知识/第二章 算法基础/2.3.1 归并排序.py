58f4575f0901f2faada17786625e09e517e07841
XiaomingSu <xiaoming.su@patpat.com>
2015-07-13 13:24:07 +0800

    O(nk+nlg(n/k))

--- 第一部分 基础知识/第二章 算法基础/2.3.1 归并排序.py
+++ 第一部分 基础知识/第二章 算法基础/2.3.1 归并排序.py
@@ -37,6 +37,7 @@ def merge_without_sentinel(llist, rlist):
     return ret
 
 
+# O(n)
 def merge(llist, rlist):
     len_l = len(llist)
     if len_l == 0:
@@ -63,7 +64,7 @@ def merge(llist, rlist):
             j += 1
     return ret
 
-
+# O( nlg(n) )
 def merge_sort(alist):
     length = len(alist)
     if length <= 1:
@@ -81,6 +82,7 @@ def merge_sort(alist):
     return merge_without_sentinel(llist, rlist)
 
 
+# n^2： which n=k
 def insertion_sort(alist):
     for i in range(1, len(alist)):
         value = alist[i]
@@ -90,18 +92,23 @@ def insertion_sort(alist):
         alist[i] = value
     return alist
 
+
 # 当归并排序的子序列较小时，使用插入排序可能能获得更好的效率
+# O( nlg(n/k) + nk )
 def merge_sort_along_with_insertion_sort(alist, threshold_to_use_insertion):
     length = len(alist)
     if length <= threshold_to_use_insertion:
+        # k^2 * n/k = nk
         return insertion_sort(alist)
 
     half = length // 2
     llist = alist[:half]
     rlist = alist[half:]
-    llist = merge_sort(llist)
-    rlist = merge_sort(rlist)
+    llist = merge_sort_along_with_insertion_sort(llist, threshold_to_use_insertion)
+    rlist = merge_sort_along_with_insertion_sort(rlist, threshold_to_use_insertion)
 
+    # 共分解为n/k个部分，因此需要lg(n/k)次合并
+    # lg(n/k) * (n+n/2+n/4+n/8..) = O( nlg(n/k) )
     return merge(llist, rlist)
 
 
@@ -113,5 +120,5 @@ if __name__ == '__main__':
     print(alist)
     print("After:")
     # alist = merge_sort(alist)
-    alist = merge_sort_along_with_insertion_sort(alist,5)
+    alist = merge_sort_along_with_insertion_sort(alist, 5)
     print(alist)
