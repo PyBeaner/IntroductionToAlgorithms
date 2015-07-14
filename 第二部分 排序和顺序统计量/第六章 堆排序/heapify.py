24298d825f2d3779d098e0a01c33c5c9cee29aa0
XiaomingSu <xiaoming.su@patpat.com>
2015-07-14 17:25:30 +0800

    heap sort

--- 第二部分 排序和顺序统计量/第六章 堆排序/heapify.py
+++ 第二部分 排序和顺序统计量/第六章 堆排序/heapify.py
@@ -2,24 +2,27 @@
 __author__ = 'PyBeaner'
 
 
+# O(lg(n))
+# height = lg(i)
 def heapify(alist, i=0):
     # 使某个结点堆化，(前提：假设该结点的子节点都已堆化）
     length = len(alist)
     l = 2 * i
     r = 2 * i + 1
     smallest = i
-    if l in alist:
-        if l <= length and alist[l] < alist[i]:
-            smallest = l
-    if r in alist:
-        if r <= length and alist[r] < alist[smallest]:
-            smallest = r
+    if l < length and alist[l] < alist[i]:
+        smallest = l
+
+    if r < length and alist[r] < alist[smallest]:
+        smallest = r
 
     if smallest != i:
         alist[i], alist[smallest] = alist[smallest], alist[i]
         heapify(alist, smallest)
 
 
+# O(nlg(n))
+# O(n) # 每个节点调用heapify的时间和高度有关，最终可得到构建一个heap的时间复杂度是和线性的
 def build_heap(alist):
     length = len(alist)
     # 从叶子上层开始逐步往上层heapify
@@ -27,10 +30,17 @@ def build_heap(alist):
         heapify(alist, i)
 
 
+def heap_sort(alist):
+    sorted_list = []
+    while alist:
+        build_heap(alist)
+        sorted_list.append(alist.pop(0))
+    return sorted_list
+
 if __name__ == '__main__':
     from random import sample
 
-    alist = sample(range(10), 10)
+    alist = sample(range(100), 10)
     print(alist)
 
     build_heap(alist)
@@ -40,3 +50,6 @@ if __name__ == '__main__':
 
     heapq.heapify(alist)
     print(alist)
+
+    sort = heap_sort(alist)
+    print(sort)
