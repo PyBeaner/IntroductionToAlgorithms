57e519131d2d21049987b248132020ef4d410603
XiaomingSu <xiaoming.su@patpat.com>
2015-07-14 13:26:07 +0800

    support for negative numbers

--- 第一部分 基础知识/第四章 分治策略/4.1 最大子数组/max-subarray-brute-force.py
+++ 第一部分 基础知识/第四章 分治策略/4.1 最大子数组/max-subarray-brute-force.py
@@ -1,15 +1,14 @@
 __author__ = 'PyBeaner'
 
-
+threshold = -10**10
 def find_max_subarray(alist):
     length = len(alist)
-
-    max_sum = 0
+    max_sum = threshold
     left = right = 0
     for i in range(length):
         left_in_loop = right_in_loop = i
         sum_in_loop = 0
-        max_sum_in_loop = 0
+        max_sum_in_loop = threshold
         for j in range(i, length):
             sum_in_loop += alist[j]
             if sum_in_loop > max_sum_in_loop:
@@ -25,6 +24,7 @@ def find_max_subarray(alist):
 if __name__ == '__main__':
     from random import sample
 
+    # alist = sample(range(-10, 0), 10)
     alist = sample(range(-10, 10), 10)
     print(alist)
     result = find_max_subarray(alist)
