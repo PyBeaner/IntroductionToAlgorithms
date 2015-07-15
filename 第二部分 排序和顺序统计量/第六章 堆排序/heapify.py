061c6baae41b52cf92ca943f897e55e4f182e7e2
PyBeaner <758046738@qq.com>
2015-07-15 08:44:37 +0800

    extract max from maxheap

--- 第二部分 排序和顺序统计量/第六章 堆排序/heapify.py
+++ 第二部分 排序和顺序统计量/第六章 堆排序/heapify.py
@@ -22,25 +22,59 @@ def heapify(alist, i=0, heap_size=None):
         heapify(alist, smallest, heap_size)
 
 
+def heapify_max(alist, i=0, heap_size=None):
+    if heap_size is None:
+        heap_size = len(alist)
+    l = 2 * i
+    r = 2 * i + 1
+    biggest = i
+    if l < heap_size and alist[l] > alist[i]:
+        biggest = l
+
+    if r < heap_size and alist[r] > alist[biggest]:
+        biggest = r
+
+    if biggest != i:
+        alist[i], alist[biggest] = alist[biggest], alist[i]
+        heapify_max(alist, biggest, heap_size)
+
+
 # O(nlg(n))
 # O(n) # 每个节点调用heapify的时间和高度有关，最终可得到构建一个heap的时间复杂度是和线性的
-def build_heap(alist):
+def build_heap(alist, maxheap=False):
     length = len(alist)
     # 从叶子上层开始逐步往上层heapify
-    for i in range(length // 2, -1, -1):
-        heapify(alist, i)
+    if maxheap:
+        for i in range(length // 2, -1, -1):
+            heapify_max(alist, i)
+    else:
+        for i in range(length // 2, -1, -1):
+            heapify(alist, i)
+
 
 # O(nlg(n))
 def heap_sort(alist):
-    build_heap(alist)
+    build_heap(alist, maxheap=True)
     heap_size = len(alist)
     for i in range(heap_size - 1, 0, -1):
         # 得到有效heap中最值
         alist[i], alist[0] = alist[0], alist[i]
         # 通过限制heap的大小，使得heapify的时候可以不影响已经得到的排好序的部分
-        heapify(alist, 0, i)
-    # 应该构建最大堆，可以避免此处的reverse
-    alist.reverse()
+        heapify_max(alist, 0, i)
+        # 应该构建最大堆，可以避免此处的reverse
+        # alist.reverse()
+
+
+def heap_extract_max(maxheap, heap_size=None):
+    if heap_size is None:
+        heap_size = len(maxheap)
+    if heap_size == 0:
+        raise Exception("heap underflow")
+    max = maxheap[0]
+    maxheap[0] = maxheap[heap_size - 1]
+    heap_size -= 1
+    heapify_max(maxheap, 0, heap_size)
+    return max
 
 
 if __name__ == '__main__':
@@ -54,3 +88,8 @@ if __name__ == '__main__':
 
     heap_sort(alist)
     print(alist)
+
+    build_heap(alist, maxheap=True)
+    print(alist)
+    for i in range(len(alist)):
+        print(heap_extract_max(alist, len(alist) - i), end=",")
