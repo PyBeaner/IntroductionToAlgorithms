524e45022e5e6c946b2eb01c1be23f99652bcdda
XiaomingSu <xiaoming.su@patpat.com>
2015-07-21 13:31:14 +0800

    length of double linked list

--- 第三部分 数据结构/第十章 基础数据结构/linked-list/double_ll.py
+++ 第三部分 数据结构/第十章 基础数据结构/linked-list/double_ll.py
@@ -13,25 +13,48 @@ class Node:
 
 class DoubleLinkedList:
     def __init__(self, head=None):
+        assert isinstance(head, Node) or head is None
         self.head = head
+        if head is None:
+            self._length = 0
+        else:
+            self._length = 1
 
     @property
     def tail(self):
         return self._getTail()
 
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, value):
+        self._length = value
+
+    def __len__(self):
+        return self.length
+
     def _getTail(self):
         tail = self.head
+        length = 0
         if self.head is not None:
+            length = 1
             while True:
                 if tail.next is None:
                     break
                 tail = tail.next
+                length += 1
+        self._length = length
         return tail
 
     def append(self, node):
         node.next = None
         node.prev = self.tail
         self.tail.next = node
+
+    def insert(self, node, pos=None):
+        pass
 
     def search(self, value):
         x = self.head
